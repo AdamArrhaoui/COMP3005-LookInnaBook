66fb8b4fd0de52a89e659f10d64741d6a64c0f5e
AdamArrhaoui <71520592+AdamArrhaoui@users.noreply.github.com>
2022-11-28 16:38:39 -0500

    Replaced module with console-menu, added basic test menu

--- main.py
+++ main.py
@@ -3,7 +3,12 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import click
-from consolemenu import ConsoleMenu
+from consolemenu import ConsoleMenu, SelectionMenu
+from consolemenu.items import SubmenuItem
+
+test_items: list[str] = ["Hunger gamer", "Hunger gamer 2", "Hunger gamer 3", "Bible 2: Electric bogaloo", "damn son the movie"]
+for i in range(5):
+    test_items += test_items
 
 
 @click.command()
@@ -15,6 +20,9 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi()
+    main_menu = ConsoleMenu("Main Menu", "Please select an option below")
+    book_menu = SelectionMenu(test_items, "Book List", "Choose da book")
+    main_menu.append_item(SubmenuItem("Go to da submenu", book_menu))
+    main_menu.show()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
